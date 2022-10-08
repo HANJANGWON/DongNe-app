bbb9d562a11f088cb6eac25349065b116029e138
HANJANGWON <wkddnjsdl00@naver.com>
2022-10-08 19:50:31 +0900

    add : profile header

--- screens/Me.tsx
+++ screens/Me.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
+import useMe from "../hooks/useMe";
 
-const Me = () => {
+const Me = ({ navigation }: any) => {
+  const { data } = useMe();
+  useEffect(() => {
+    navigation.setOptions({
+      title: data?.me?.username,
+    });
+  }, []);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Me</Text>
--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 
-const Profile = () => {
+const Profile = ({ navigation, route }: any) => {
+  useEffect(() => {
+    if (route?.params?.username) {
+      navigation.setOptions({
+        title: route.params.username,
+      });
+    }
+  }, []);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Someones Profile</Text>
